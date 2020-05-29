d890ea1e8a864736f8ba68e074e2c5cf227d5116
NikhilBhutani <niks.bhutani@gmail.com>
2020-05-30 02:04:42 +0530

    Build a DAPP on supply chain for automobile industry

---
+++ Architect a Blockchain Supply Chain Solution/Part 2(Implementation)/project-6/truffle.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "0334e88931064e49b8e4dc097a237c4d";
+
+const fs = require('fs');
+const mnemonic = 'Enter Metamask Secret Key here'
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24"
+    }
+  }
+
+};
